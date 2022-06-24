62bd1c51a62e4a8e055de37ec2b4fcf0e328ab78
jw397 <jw397@126.com>
2022-06-24 19:17:07 +0800

    fix: defineProperty to Reflect

--- mod.ts
+++ mod.ts
@@ -1,1 +1,15 @@
-export * as Reflect from "./Reflect.ts";
+// deno-lint-ignore-file no-explicit-any
+import * as ref from "./Reflect.ts";
+
+for (const key in ref) {
+  const value = (ref as any)[key];
+  if (typeof value === "function") {
+    Object.defineProperty(Reflect, key, {
+      configurable: true,
+      writable: true,
+      value,
+    });
+  }
+}
+
+export { ref as Reflect };
